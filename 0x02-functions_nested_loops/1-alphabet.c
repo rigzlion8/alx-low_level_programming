66ed8798960ae78314d903f753b5b7374dba028e
Reagan Musa <reagan.musav@gmail.com>
2022-12-15 18:56:15 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "headers.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase, followed by a new line
+ *
+ **/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar(10);
+}
