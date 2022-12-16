389557b177662a6bb1087e6fc9a9902e81717138
Reagan Musa <reagan.musav@gmail.com>
2022-12-16 09:47:37 -0800

    fixed 0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,26 +4,3 @@
 int _putchar(char c)
 #endif
 
-
-
-
-
-
-
-
-putchar(char c);
-/*void print_alphabet(void);
-/**
- * print_alphabet -> prints the lowecase alphabets
- *
- *
-void print_alphabet(void)
-{
-        int c
-
-	for (c = 'a'; c <= 'z'; c++)
-        {
-                _putchar(c);
-	        _putchar('\n');
-	}
-}*/
