9a6b9409134dc28d4d2fb0ccd3b8175df40d4906
Reagan Musa <reagan.musav@gmail.com>
2022-12-16 10:04:03 -0800

    putthe car

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,16 +5,14 @@
  *
  * Return: 0
  **/
-int main(void)
+void print_alphabet(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+        {
+                _putchar(c);
+        }
+        _putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/headers.h
+++ 0x02-functions_nested_loops/headers.h
@@ -1,2 +1,1 @@
-/*#include "/root/alx-low_level_programming/0x02-functions_nested_loops/headers.h"*/
 void print_alphabet(void);
