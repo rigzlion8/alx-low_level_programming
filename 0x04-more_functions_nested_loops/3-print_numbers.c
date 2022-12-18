13caf34f4b877aa01a57090592e678c9057850a8
Reagan Musa <reagan.musav@gmail.com>
2022-12-18 09:30:10 -0800

    fix 3-print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * *print_numbers - print 0123456789
- * * owned by Bwave ICT
- * *Return: void
- * */
+ * print_numbers - print 0123456789
+ * owned by Bwave ICT
+ * Return: void
+ *
+ **/
 
 void print_numbers(void)
 {
