fbcb32ae063c6c441a550615a6fcd131a7d281bf
Reagan Musa <reagan.musav@gmail.com>
2022-12-17 22:14:55 -0800

    hotfix-final-0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints alphabet in lowercase, followed by a new line
+ * main - Print _putchar
+ * Return: Always 0
  *
- * Return: 0
  **/
-void print_alphabet(void)
+int main(void)
 {
-	int c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-		_putchar('\n');
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
