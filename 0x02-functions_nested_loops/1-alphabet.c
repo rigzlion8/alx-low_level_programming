e0bc8cab5e2cbed8b25a40af897a151c9242c19f
Reagan Musa <reagan.musav@gmail.com>
2022-12-17 22:26:42 -0800

    1-alphabet-finale

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,10 @@
+#include "main.h"
+
 /**
  * print_alphabet - prints alphabet in lowercase, followed by a new line
  *
  **/
+
 void print_alphabet(void)
 {
 	char c;
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,10 +1,11 @@
-#include "./../0x02-functions_nested_loops/headers.h"
+#include "main.h"
 
 /*
  * main - check the code
  *
  * Return: Always 0.
  */
+
 void print_alphabet(void);
 
 int main(void)
