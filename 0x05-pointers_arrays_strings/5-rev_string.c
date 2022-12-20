1b2505941d92d6e7301797d5e04b41d963aa66df
Reagan Musa <reagan.musav@gmail.com>
2022-12-20 15:04:44 -0800

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+/**
+ * rev_string - function that reverses a string.
+ * @s: sting to reverse
+ *
+ * Return: void
+ **/
+
+void rev_string(char *s)
+{
+	int l, i, j;
+	char temp;
+
+	l = 0;
+	while (l >= 0)
+	{
+		if (s[l] == '\0')
+			break;
+		l++;
+	}
+	for (i = 0; i < (l - 1);
+		       	i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(s + j);
+			*(s + j) = *(s + (j - 1));
+			*(s + (j - 1)) = temp;
+		}
+	}
+}
