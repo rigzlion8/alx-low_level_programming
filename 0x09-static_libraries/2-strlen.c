465312ffbf6bee2e8ed303a08581c1ad68313bf3
Reagan Musa <reagan.musav@gmail.com>
2023-01-09 12:04:20 -0800

    0x09-libmy

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,1 +1,16 @@
-#include "main.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: string to check
+ *
+ * Return: Integer
+ **/
+
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (*(s + counter) != '\0')
+		counter++;
+	return (counter);
+}
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,1 +1,18 @@
 #include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Write the character c to stdout
+ * @c: The character to be printed.
+ *
+ * Return - On success return 1
+ * Return - On error return -1. 
+ *
+ * errno is set appropriately.
+ *
+ **/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}	
